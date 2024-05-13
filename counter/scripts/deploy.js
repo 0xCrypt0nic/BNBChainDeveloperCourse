bbfdd98d4ec0d045bc4fdd97da7316487c0f1823
0xCrypt0nic <n.greneche45@gmail.com>
2024-05-13 15:18:58 +0200

    Counter project updated

---
+++ counter/scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  // We get contract to deploy
+
+  const Counter = await ethers.getContractFactory("Counter");
+  const counter = await Counter.deploy();
+
+  await counter.deployed();
+
+  console.log("Counter deployed to ", counter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
