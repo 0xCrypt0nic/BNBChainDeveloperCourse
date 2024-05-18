e2ca52cca742fa2c427eef8611ed1c448c35101e
0xCrypt0nic <n.greneche45@gmail.com>
2024-05-18 23:37:40 +0200

    RPS DApp started

---
+++ RPS_DApp/contracts/RPS.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@binance-oracle/binance-oracle-starter/contracts/mock/VRFConsumerBase.sol";
+import "@binance-oracle/binance-oracle-starter/contracts/interfaces/VRFCoordinatorInterface.sol";
+
+contract RPS {
+    constructor() {}
+}
