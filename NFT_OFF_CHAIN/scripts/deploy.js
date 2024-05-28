55660ce34adc5efb53c35703251d9c8502448eb5
0xCrypt0nic <n.greneche45@gmail.com>
2024-05-28 22:27:41 +0200

    NFT with OffChain Metadata project finished

---
+++ NFT_OFF_CHAIN/scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account : ", deployer.address);
+  const token = await ethers.deployContract("IconsNFT");
+  console.log("Token address:", await token.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
