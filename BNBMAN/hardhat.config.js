b2abb6e2d8e244d2af26f55bc67ec05fde08368c
0xCrypt0nic <n.greneche45@gmail.com>
2024-05-29 22:37:55 +0200

    BNBMan smartcontract finished

--- BNBMAN/hardhat.config.js
+++ BNBMAN/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    bnbTestnet: {
+      url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545/",
+      chainId: 97,
+      accounts: [SECRET_KEY],
+    },
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+    },
+  },
 };
